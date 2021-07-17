dfae5fad6285a3a2b7197d916c6fb4a2a87e2f56
ELKABOOOS <85698850+ELKABOOOS@users.noreply.github.com>
2021-07-17 02:29:10 +0200

    Add files via upload

--- BANDA.lua
+++ BANDA.lua
@@ -4358,6 +4358,7 @@ keyboard.inline_keyboard = {
 local msg_id = msg.id_/2097152/0.5 
 https.request("https://api.telegram.org/bot"..token..'/sendVoice?chat_id=' .. msg.chat_id_ .. '&voice=https://t.me/ROGE_Y/'..ght..'&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
 end
+end
 
 if text == "استوري" and not  database:get(bot_id.."sing:for:me"..msg.chat_id_) then 
 ght = math.random(1,24); 
